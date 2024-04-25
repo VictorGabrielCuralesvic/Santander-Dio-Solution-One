5b6b5ad183f50881f48c17b375fb410c3b6228b9
Nome <miranhaomi@gmail.com>
2024-04-24 21:58:45 -0300

    Create ContaTerminal.java

---
+++ ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("digite a agencia");
+        String agencia = scanner.nextLine();
+
+        System.out.println("digite o número da conta");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("nome do cliente");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("digite o saldo");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+    }
+}
